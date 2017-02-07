a0199b54ab87a6178afd7be0f2262e3834406bfd
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2017-02-07 11:36:20 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,9 +1,29 @@
-export default function createStore(reducer){
-
-}
+// export default function createStore(reducer){
+//
+// }
 
 // solution
+
+
 export default function createStore(reducer){
+  let state
+
+  function dispatch(action){
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state
+  }
+
+  dispatch({type: '@@INIT'})
+
+  return {
+    dispatch: dispatch,
+    getState: getState
+  }
+
 }
 
 function render(){
